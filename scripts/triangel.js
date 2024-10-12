f4e481ab2e4854a7bc5a289add841a7284e8af64
Sapa <nursapa2001@gmail.com>
2024-10-12 18:16:37 +0600

    added triangle calculatiion part

---
+++ scripts/triangel.js
@@ -0,0 +1,11 @@
+function calculateTriangle(){
+    const triangleBaseInput = document.getElementById('triangle-base');
+    const triangleBaseText = triangleBaseInput.value;
+    const base = parseFloat(triangleBaseText);
+    console.log(base);
+
+    const triangleHeightInput = document.getElementById('triangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText);
+    console.log(height);
+}
